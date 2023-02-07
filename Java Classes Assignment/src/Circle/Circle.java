33de882ba6f78d9391423aa3a10091277ef1adfa
Kiana <94536969+Kiana-ko@users.noreply.github.com>
2023-02-06 22:31:34 -0800

    Add files via upload

---
+++ Java Classes Assignment/src/Circle/Circle.java
@@ -0,0 +1,25 @@
+// Creating a class called Circle:
+package Circle;
+//Creating a class and passing  2 relevant fields lik radius, diameter in it:
+
+public class Circle {
+    public int diameter;
+    public int radius;
+
+    // Creating the circle cunstroctor
+    public Circle(int diameter,int radius) {
+        this.diameter = diameter;
+        this.radius = radius;
+    }
+
+    public void  getArea() {
+        //return //
+    }
+
+    public void getParameter () {
+        //return //
+    }
+
+}
+
+
---
+++ Java Classes Assignment/src/Cylinder/Cylinder.java
@@ -0,0 +1,23 @@
+// Creating the circle called Cylinder
+package Cylinder;
+
+public class Cylinder {
+        public int diameter;
+        public int radius;
+
+        // Creating the circle cunstroctor
+        public Cylinder (int diameter,int radius) {
+            this.diameter = diameter;
+            this.radius = radius;
+    }
+
+
+    public void getVolume() {
+        //return //
+    }
+}
+
+
+
+
+
